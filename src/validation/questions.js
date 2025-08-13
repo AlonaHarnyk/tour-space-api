19c531c8a3aa7be27e650229cee52e2ffc05a294
Alona Harnyk <alyonagarnyk@gmail.com>
2025-08-13 09:05:31 +0300

    add schemas

--- src/validation/questions.js
+++ src/validation/questions.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const questionSchema = Joi.object({
   tourName: Joi.string().required(),
-  userName: Joi.string().required(),
+  userName: Joi.string().min(1).max(100).required(),
   phoneNumber: Joi.string().required(),
-  question: Joi.string().required(),
+  question: Joi.string().min(10).max(300).required(),
 });
--- src/validation/review.js
+++ src/validation/review.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const reviewSchema = Joi.object({
   rating: Joi.number().min(1).max(5).required(),
-  userName: Joi.string().required(),
-  tourCountry: Joi.string().required(),
-  review: Joi.string().required(),
+  userName: Joi.string().min(1).max(100).required(),
+  tourCountry: Joi.string().min(1).max(100).required(),
+  review: Joi.string().min(10).max(300).required(),
 });
